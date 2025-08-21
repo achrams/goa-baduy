df16c9ca7e707707d05442ca59e0e1f41f914906
achrams <ramadhanovic@gmail.com>
2025-08-21 14:00:46 +0700

    done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <footer class="w-full h-fit bg-gray-900 py-4 px-6 text-white text-center">
+    <footer class="w-full h-fit bg-gray-900 py-4 px-6 text-white text-center flex items-center justify-between">
+      <div class="w-14"></div>
       <p class="text-sm">© 2025 Goa Baduy Palayangan. All rights reserved.</p>
+      <div class="w-6 cursor-pointer" @click="toPage('/admin')"><img src="../assets/icons/admin.png" alt="admin"></div>
     </footer>
   </div>
 </template>
 <script setup>
+import { useRouter } from 'vue-router';
+
+const toPage = (path) => {
+  useRouter().push(path)
+}
 </script>
